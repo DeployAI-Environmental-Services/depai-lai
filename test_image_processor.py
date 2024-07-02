7cab3bba90f9f33b9a928af84658789e48fa2a26
albughdadim <mohanad.usm@gmail.com>
2024-07-02 14:32:11 +0200

    first commit

---
+++ test_image_processor.py
@@ -0,0 +1,30 @@
+import grpc
+import pytest
+from concurrent import futures
+import model_pb2
+import model_pb2_grpc
+
+
+@pytest.fixture(scope="module")
+def grpc_stub():
+    channel = grpc.insecure_channel("localhost:8061")
+    stub = model_pb2_grpc.ImageProcessorStub(channel)
+    yield stub
+    channel.close()
+
+
+def test_process_image(grpc_stub):
+    request = model_pb2.ImageRequest(
+        images=[
+            model_pb2.ImageData(image_path="/data/test_img_10b.tif", offset=-1000),
+        ]
+    )
+
+    response = grpc_stub.ProcessImage(request)
+    assert response.results
+    for result in response.results:
+        assert result.image_path in [
+            "/data/test_img_10b.tif",
+            "/data/test_img_10b2.tif",
+        ]
+        assert result.processed is True
